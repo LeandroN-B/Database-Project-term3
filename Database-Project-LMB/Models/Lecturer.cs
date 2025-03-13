82abeef1d8ddb5715002003b56bcfae0966c5f99
MatheusFracasso <725283@student.inholland.nl>
2025-03-13 13:39:24 +0100

    Update Lecturer.cs

---
+++ Database-Project-LMB/Models/Lecturer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Database_Project_LMB.Models
+{
+    public class Lecturer
+    {
+        public int LecturerID { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } //= string.Empty;
+        public int RoomID { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+
+        public int Age
+        {
+            get
+            {
+                int age = DateTime.Now.Year - DateOfBirth.Year;
+                if (DateTime.Now < DateOfBirth.AddYears(age)) age--;
+                return age;
+            }
+        }
+    }
+}
